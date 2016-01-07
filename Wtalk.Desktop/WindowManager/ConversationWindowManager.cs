eec77f135b65cc91fe89ee1104147341d506d9ef
madagaga <le.malgache@gmail.com>
2016-01-07 15:38:34 +0100

    handle alt+Enter for new line

--- Wtalk.Desktop/WindowManager/ConversationWindowManager.cs
+++ Wtalk.Desktop/WindowManager/ConversationWindowManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Wtalk.Desktop.ViewModel;
 using Wtalk.Desktop.Extension;
+using System.Windows.Input;
 
 namespace Wtalk.Desktop.WindowManager
 {
@@ -34,7 +35,7 @@ namespace Wtalk.Desktop.WindowManager
                 Window = new ConversationWindow();
                 Window.Activated+=Window_Activated;
                 Window.Closing += Window_Closing;
-                Window.message_textbox.KeyDown+=message_textbox_KeyDown;
+                Window.message_textbox.PreviewKeyDown+=message_textbox_KeyDown;
                 Window.DataContext = ViewModel;
             }
         }
@@ -56,14 +57,20 @@ namespace Wtalk.Desktop.WindowManager
 
         private void message_textbox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            if (e.Key == System.Windows.Input.Key.Enter)
+            System.Windows.Controls.TextBox box = sender as System.Windows.Controls.TextBox;
+            if (Keyboard.Modifiers == ModifierKeys.Alt && Keyboard.IsKeyDown(Key.Enter))
+            {
+                box.Text += Environment.NewLine;
+                box.SelectionStart = box.Text.Length;
+                e.Handled = true;
+            }
+            else if (Keyboard.IsKeyDown(Key.Enter))
             {                
                 ((MainViewModel)System.Windows.Application.Current.MainWindow.DataContext).SetPresenceCommand.Execute(null);
-                System.Windows.Controls.TextBox box = sender as System.Windows.Controls.TextBox;
+                
                 ViewModel.SendMessageCommand.Execute(box.Text);
                 box.Text = string.Empty;
-                
-
+                e.Handled = true;
             }
         }
 
