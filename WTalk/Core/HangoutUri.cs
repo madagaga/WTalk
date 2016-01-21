2d431d3e77c67e6ba73ca2039b154d1feb9ec1ad
madagaga <le.malgache@gmail.com>
2016-01-21 17:44:38 +0100

    uri update

--- WTalk/Core/HangoutUri.cs
+++ WTalk/Core/HangoutUri.cs
@@ -11,10 +11,10 @@ namespace WTalk
         public const string ORIGIN_URL = "https://talkgadget.google.com";
         //public const string ORIGIN_URL = "https://hangouts.google.com";
         public const string IMAGE_UPLOAD_URL = "http://docs.google.com/upload/photos/resumable";
-        //public const string PVT_TOKEN_URL = "https://hangouts.google.com/webchat/extension-start";
-        public const string PVT_TOKEN_URL = "https://talkgadget.google.com/talkgadget/_/extension-start";
-        //public const string CHAT_INIT_URL = "https://hangouts.google.com/webchat/u/0/load";
-        public const string CHAT_INIT_URL = "https://talkgadget.google.com/u/0/talkgadget/_/chat";
+        public const string PVT_TOKEN_URL = "https://hangouts.google.com/webchat/extension-start";
+        //public const string PVT_TOKEN_URL = "https://talkgadget.google.com/talkgadget/_/extension-start";
+        public const string CHAT_INIT_URL = "https://hangouts.google.com/webchat/u/0/load";
+        //public const string CHAT_INIT_URL = "https://talkgadget.google.com/u/0/talkgadget/_/chat";
         public const string CHANNEL_URL = "https://0.client-channel.google.com/client-channel/";
         public const string CHAT_SERVER_URL = "https://clients6.google.com/chat/v1/";
 
